5235ef220180b4be3efb5560602a057a57f42ebb
Alma-pineiro <alma.pineiro1@upr.edu>
2026-01-29 13:37:12 -0400

    final

--- Lab1/L1/src/functions.c
+++ Lab1/L1/src/functions.c
@@ -1,3 +1,5 @@
+#include "../include/functions.h"
+
 #include <ctype.h>
 
 /**
--- Lab1/L1/src/main.c
+++ Lab1/L1/src/main.c
@@ -2,32 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/functions.h"
+
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
+  if (argc != 3) {
     printf("Error: Invalid number of arguments.\n");
   }
 
-  if (argv[2] != 'C' && argv[2] != 'F' && argv[2] != 'K') {
+  if (argv[2][0] != 'C' && argv[2][0] != 'F' && argv[2][0] != 'K') {
     printf("Error: Invalid unit.\n");
   }
 
-  if (argc == 'C') {
+  if (argv[2][0] == 'C') {
     float celsius = atof(argv[1]);
     float fahrenheit = CelsiusToFahrenheit(celsius);
     float kelvin = CelsiusToKelvin(celsius);
-    printf("Fahrenheit: %.2f\n", fahrenheit);
-    printf("Kelvin: %.2f\n", kelvin);
-  } else if (argc == 'F') {
+    printf("%s %c is %.2f F and %.2f K\n", argv[1], argv[2][0], fahrenheit,
+           kelvin);
+
+  } else if (argv[2][0] == 'F') {
     float fahrenheit = atof(argv[1]);
     float celsius = FahrenheitToCelsius(fahrenheit);
     float kelvin = CelsiusToKelvin(celsius);
-    printf("Celsius: %.2f\n", celsius);
-    printf("Kelvin: %.2f\n", kelvin);
-  } else if (argc == 'K') {
+    printf("%s %c is %.2f C and %.2f K\n", argv[1], argv[2][0], celsius,
+           kelvin);
+
+  } else if (argv[2][0] == 'K') {
     float kelvin = atof(argv[1]);
     float celsius = KelvinToCelsius(kelvin);
     float fahrenheit = CelsiusToFahrenheit(celsius);
-    printf("Celsius: %.2f\n", celsius);
-    printf("Fahrenheit: %.2f\n", fahrenheit);
+    printf("%s %c is %.2f C and %.2f F\n", argv[1], argv[2][0], celsius,
+           fahrenheit);
   }
 }
